80203c0817c4937cab7443454600b5436ca9aaa5
louis <nguyenlouis4@gmail.com>
2025-07-21 22:59:32 +1000

    Initial Commit

---
+++ Source/HexedUnreal/Helpers/DebugHelper.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+namespace DebugHelper
+{
+	static void Log(const FString& Message)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, Message);
+	}
+
+	static void Log(const float FloatMessage)
+	{
+		const FString OutMessage = FString::Printf(TEXT("%f"), FloatMessage);
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, OutMessage);
+	}
+
+	static void Log(const double DoubleMessage)
+	{
+		const FString OutMessage = FString::Printf(TEXT("%f"), DoubleMessage);
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, OutMessage);
+	}
+	
+	static void Log(const int IntMessage)
+	{
+		const FString OutMessage = FString::Printf(TEXT("%d"), IntMessage);
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, OutMessage);
+	}
+	
+}
---
+++ Source/HexedUnreal/HexedUnreal.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HexedUnreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HexedUnreal, "HexedUnreal" );
+
+DEFINE_LOG_CATEGORY(LogHexedUnreal)
+ 
